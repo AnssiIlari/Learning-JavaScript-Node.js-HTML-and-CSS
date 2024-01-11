1baa087500d2ceeb54f09dfe9f4682a69e67cbf9
Anssi Laitinen <korbikoski@gmail.com>
2024-01-11 11:06:14 +0200

    React.js init

---
+++ React.js (Spring 2024)/helloreact/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to my site</h1>
+       <p>This is my first React App</p>
+    </div>
+  );
+}
+
+export default App
