6714eae0d17292a69a3c346f15ff3e782a397fb1
Anssi Laitinen <korbikoski@gmail.com>
2024-01-18 09:31:40 +0200

    userlist with image

---
+++ React.js (Spring 2024)/Open exercises/Userlist/README.md
@@ -0,0 +1,3 @@
+# REACT & REST
+
+## Gets users from REST api and inserts them into a html element w/ image
---
+++ React.js (Spring 2024)/Open exercises/Userlist/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch('https://reqres.in/api/users')
+      .then(response => response.json())
+      .then(resData => setUsers(resData.data))
+      .catch(err => console.error(err))
+  }, []);
+
+  return (
+    <>
+      <table>
+        <tbody>
+          {
+            users.map((user) =>
+              <tr key={user.id}>
+                <td>{user.first_name}</td>
+                <td>{user.last_name}</td>
+                <td>{user.email}</td>
+                <td><img src={user.avatar} alt={user.first_name} /></td>
+              </tr>
+            )
+          }
+        </tbody>
+      </table>
+
+    </>
+  )
+}
+
+export default App
