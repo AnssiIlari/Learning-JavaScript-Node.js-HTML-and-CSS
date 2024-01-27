7c43afa42fecc3e337057edafc70ed5ffd3c0f80
Anssi Laitinen <korbikoski@gmail.com>
2024-01-27 16:23:56 +0200

    delete method added

--- React.js (Spring 2024)/firebasetodo/src/App.jsx
+++ React.js (Spring 2024)/firebasetodo/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState, useEffect } from 'react'
 import { AgGridReact } from 'ag-grid-react';
 import './App.css'
-
 import "ag-grid-community/styles/ag-grid.css"; // Core CSS
 import "ag-grid-community/styles/ag-theme-quartz.css"; // Theme
-
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 import AddTodo from './AddTodo';
 
@@ -18,6 +18,15 @@ function App() {
     { field: 'description', sortable: true, filter: true },
     { field: 'date', sortable: true, filter: true },
     { field: 'priority', sortable: true, filter: true },
+    { 
+      headerName: '',
+      field: 'id',
+      width: 90,
+      cellRenderer: params => 
+      <IconButton onClick={() => deleteTodo(params.value)} size="small" color="error">
+        <DeleteIcon />
+      </IconButton> 
+    }
   ]
 
   useEffect(() => {
@@ -29,9 +38,17 @@ function App() {
   const fetchItems = () => {
     fetch('https://todolist-2d959-default-rtdb.europe-west1.firebasedatabase.app/items/.json')
       .then(response => response.json())
-      .then(data => setTodos(Object.values(data)))
+      .then(data => addKeys(data))
       .catch(err => console.error(err))
   }
+
+    // Add keys to the todo objects
+    const addKeys = (data) => {
+      const keys = Object.keys(data);
+      const valueKeys = Object.values(data).map((item, index) => 
+      Object.defineProperty(item, 'id', {value: keys[index]}));
+      setTodos(valueKeys);
+    }
 
   const addTodo = (newTodo) => {
     fetch('https://todolist-2d959-default-rtdb.europe-west1.firebasedatabase.app/items/.json',
@@ -42,6 +59,16 @@ function App() {
     .then(response => fetchItems())
     .catch(err => console.error(err))
   }
+
+  const deleteTodo = (id) => {
+    fetch(`https://todolist-2d959-default-rtdb.europe-west1.firebasedatabase.app/items/${id}.json`,
+    {
+      method: 'DELETE',
+    })
+    .then(response => fetchItems())
+    .catch(err => console.error(err))
+  }
+
 
 
   return (
@@ -54,7 +81,7 @@ function App() {
         </Toolbar>
       </AppBar>
       <AddTodo addTodo={addTodo} />
-      <div className="ag-theme-quartz" style={{ height: 200, width: 650 }}>
+      <div className="ag-theme-quartz" style={{ height: 200, width: 700 }}>
         <AgGridReact
           rowData={todos}
           columnDefs={columnDefs}
